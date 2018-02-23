dc78a505e7d11e9b9bc2c3d32880933ebc266a39
linxinzhe <linxinzhechn@qq.com>
2018-02-23 15:20:49 +0800

    fix(lib): tomcrypt header

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -rf build
 mkdir -p build
-gcc -c src/program_ecc.c -o build/program_ecc.o
+gcc -c src/program_ecc.c -Iinclude -o build/program_ecc.o
 
 gcc build/program_ecc.o lib/libtomcrypt.a -o build/program_ecc.elf
 
