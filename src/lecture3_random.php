42cc157560bcd27d76ab6ac0c395bb740b45c646
linxinzhe <linxinzhechn@qq.com>
2018-02-24 11:58:24 +0800

    fix(crypt): file path

--- src/lecture3_random.php
+++ src/lecture3_random.php
@@ -3,7 +3,7 @@
 
 // random.txt is cropped from /dev/urandom
 // BASH: cat /dev/urandom | base64 | head -n 10 -> random
-$random = file_get_contents("./random.txt");
+$random = file_get_contents("../random.txt");
 $random = base64_decode($random);
 var_dump(strlen($random));
 
